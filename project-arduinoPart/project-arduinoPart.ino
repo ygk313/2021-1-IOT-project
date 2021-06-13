48801c93b282764bd51b3cc13744d5aa726c4e5f
ygk313 <50168972+ygk313@users.noreply.github.com>
2021-06-13 19:45:44 +0900

    final

--- project-arduinoPart/project-arduinoPart.ino
+++ project-arduinoPart/project-arduinoPart.ino
@@ -1,22 +1,77 @@
-#include "DHT.h"
-#define DHTPIN 8
-#define DHTTYPE DHT11
+//물을 주고 있는 지 확인하는 LED 핀
+int led_pin = 8;
+//토양 수분 센서 값
+int soil = A0;
 
-DHT dht(DHTPIN, DHTTYPE);
+//모터 드라이버 출력 핀모드
+int pump_A= 10;
+int pump_B = 6;
 
 void setup() {
+  pinMode(led_pin, OUTPUT);
+  pinMode(pump_A, OUTPUT);
+  pinMode(pump_B, OUTPUT);
+  pinMode(soil, INPUT);
+  
   Serial.begin(115200);
   Serial1.begin(115200);
-  dht.begin();
-
+  //버퍼 비우기
+  Serial1.flush();
 }
+//Serial통신으로 받는 값
+int data = 0;
 
 void loop() {
-  int humidity = dht.readHumidity();
-  Serial.print("현재 습도:");
-  Serial.print(humidity);
-  Serial.print("%");
-  Serial.println();
-  Serial1.write(humidity);
-  delay(1000);
+  //토양수분센서 아날로그 입력
+  int soil = analogRead(soil);
+  // 현재 토양 수분량 Node MCU에 전달할 변수
+  int current_soil = 0;
+  Serial.println(soil);
+
+  //버퍼 비우기
+  Serial1.flush();
+
+  // 0(습합)~1023(건조함) 450 이하면 - 습함
+  if (soil <= 450){
+    Serial1.write(2);
+    current_soil = 2;
+  }
+  // 450이상 1000이하 - 적당함
+  else if (450 < soil && soil<=1000){
+    Serial1.write(1);
+    current_soil = 1;
+  }
+  //1000이상 - 건조함
+  else if (soil>1000){
+    Serial1.write(0);
+    current_soil = 0;
+  }
+
+  //Node MCU에서 전달받은 값이 있는 경우
+  if (Serial1.available()){
+    data = Serial1.read();
+    Serial1.flush();
+    Serial.print("data:");
+    Serial.println(data);
+  }
+  //Node MCU에서 전달 받은 값이 1이면,
+  if (data == 1){
+    //led 켜기
+    digitalWrite(led_pin, HIGH);
+    //워터 펌프 정방향으로 돌리기
+    digitalWrite(pump_A, HIGH);
+    digitalWrite(pump_B,LOW);
+    //3초 동안 쉬기
+    delay(3000);
+    //워터 펌프 멈추기
+    digitalWrite(pump_A, LOW);
+    digitalWrite(pump_B, LOW);
+    //led 끄기
+    digitalWrite(led_pin, LOW);
+    
+    data = 0;
+  }
+  else if (data == 0){
+    digitalWrite(led_pin,LOW);
+  }
 }
--- project-nodemcu/project-nodemcu.ino
+++ project-nodemcu/project-nodemcu.ino
@@ -1,27 +1,34 @@
 #include <WiFi.h>
 #include <WiFiClient.h>
 #include "WiFiAP.h"
+#include "DHT.h"
+#define DHTPIN 19
+#define DHTTYPE DHT11
+
+//온습도 센서
+DHT dht(DHTPIN, DHTTYPE);
+
+//물주기 변수
+int watering = 0;
+int soilVal = 0;
 
 const char* ssid     = "iptime303";
 const char* password = "9809191124";
 
 WiFiServer server(80);
 
-int getHumidity(){
-  int humidity = 0;
-  if (Serial2.available()){
-    humidity = Serial2.read();
-    Serial.println(humidity);
-  } 
-  return humidity;
-}
-
 void setup() {
+  //온습도 센서 연결
+  pinMode(4, INPUT);
+  
   Serial.begin(115200);
   Serial2.begin(115200);
-  
+  dht.begin();
+
+  //와이파이 연결
   WiFi.begin(ssid, password);
 
+  //와이파이 연결 기다리기
   while(WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
@@ -39,6 +46,7 @@ void setup() {
 }
 
 void loop() {
+  //client 생성
   WiFiClient client = server.available();
   if(!client.connected()) return;
 
@@ -48,40 +56,97 @@ void loop() {
   String request = client.readStringUntil('\r');
   Serial.println("request: ");
   Serial.println(request);
+  
+  Serial.print("watering:");
+  //만약 물을 주는 링크를 눌렀다면
+  if (request.indexOf(F("/Y"))!=-1){
+    watering = 1;
+    Serial.println("1");
+  }
+  //만약 물주는 링크를 누르지 않았다면,
+  else if (request.indexOf(F("/N")) != -1){
+    watering = 0;
+    Serial.println("0");
+  }
+  else{
+    Serial.println(F("invalid request"));
+  }
 
   while(client.available()) {
     client.read();
   }
-
-  int humidity = getHumidity();
-  float temp = 150.0;
-
+  
+  int humidity = dht.readHumidity();
+  float temperature = dht.readTemperature();
+  if (Serial2.available()){
+    soilVal = Serial2.read();
+    Serial.println(soilVal);
+  }
+  
   Serial.print("Humidity : ");
-  Serial.print(humidity);
-  Serial.print(" Temperature : ");
-  Serial.print(temp);
-  Serial.println(" C");
-
+  Serial.println(humidity);
+  Serial.print("Temperature: ");
+  Serial.println(temperature);
+  
+  Serial.print("SoilHumidity:");
+  Serial.println(soilVal);
+  
   client.print("HTTP/1.1 200 OK");
   client.print("Content-Type: text/html\r\n\r\n");
   client.print("<!DOCTYPE HTML>");
   client.print("<html>");
   client.print("<head>");
-  client.print("<meta charset=\"UTF-8\" http-equiv=\"refresh\" content=\"1\">");
-  client.print("<title>DHT sensor test webpage</title>");
+  client.print("<meta charset=\"UTF-8\">");
+  client.print("<title>몬스테라 키우기 :) </title>");
   client.print("</head>");
   client.print("<body>");
-  client.print("<h2>DHT sensor test webpage</h2>");
+  client.print("<h2>몬스테라 키우기 :) </h2>");
   client.print("<br>");
-  client.print("Temperature : ");
-  client.print(temp);
-  client.print(" C");
+  client.print("<h4>몬스테라는 공기를 정화하는 관엽 식물이다.<br>잎의 모양도 예뻐 인테리어를 하기에도 좋다.</h4>");
+  client.print("<br> 몬스테라의 적정 습도: 70%이상");
+  client.print("<br> 몬스테라의 적정 온도: 13도 ~ 20도");
+  client.print("<br> 몬스테라의 물 주기: 봄, 여름, 가을은 흙은 촉촉하게 유지, 겨울은 토양의 표면이 마르면 주기");
   client.print("<br>");
-  client.print("현재 습도 : ");
+  client.print("<br><hr>");
+  client.print("<br><h2> 실내 온습도 및 토양의 습도</h2>");
+  client.print("<br>현재 공기의 습도 : ");
   client.print(humidity);
   client.print(" %");
+  client.print("<br> 현재 실내 온도 : ");
+  client.print(temperature);
+  client.print("도");
+  client.print("<br> 현재 토양의 습도 : ");
+  if (soilVal == 0){
+    client.print("건조합니다.");
+  }
+  else if (soilVal == 1){
+    client.print("적당합니다.");
+  }
+  else if (soilVal == 2){
+    client.print("습합니다.");
+  }
+
+  client.print("<br>");
+ 
+  client.print("<br><hr>");
+  client.print("<br><h2>물주기를 선택해보자</h2>");
+  client.print(F("<br> <a href='http://"));
+  client.print(WiFi.localIP());
+  client.print(F("/Y'>물주기</a>를 원한다면 누르세요! "));
+  client.print(F("아니면 <a href='http://"));
+  client.print(WiFi.localIP());
+  client.print(F("/N'>물 주지 않기</a>를 원한다면 누르세요!"));
   client.print("</body>");
   client.print("</html>");
 
+  Serial2.flush();
+  if (watering == 0){
+   Serial2.write(0); 
+  }
+  else if (watering == 1){ 
+    Serial2.write(1);   
+  }
+  
+  Serial.println();
   Serial.println("client connect end");
 }
